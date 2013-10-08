5001cc0d89b055b6746087fc4b7b266324e19d29
ring-ailing-ting <mimi121503@gmail.com>
2013-10-07 23:47:25 -0400

    Javadoced

--- MULE/src/MainScreen.java
+++ MULE/src/MainScreen.java
@@ -5,6 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*
+* This is the MainScreen JFrame class that will show the logo and allow you to continue to settings selection
+* @author Kushal Nigam
+* @ Version 1.0 10/7/2013
+*/
+
 public class MainScreen extends JFrame{
 	JLabel title;
 	JButton start;
@@ -12,6 +18,9 @@ public class MainScreen extends JFrame{
 	private static final int WIDTH = 580; //I used same dimensions as Wei for consistancy
 	private static final int HEIGHT = 600; 
 	
+	/*
+	* The constructor for this class that shows the logo and sets up basic settings.
+	*/
 	public MainScreen(){
 		title = new JLabel(logo);
 		start = new JButton("Start Game");
@@ -29,6 +38,9 @@ public class MainScreen extends JFrame{
 
 	}
 	
+	/*
+	* A class that leads you to the initial settings screen when selected
+	*/
 	public class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			initial settings = new initial();
