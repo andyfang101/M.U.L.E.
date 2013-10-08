57aaa452694a087ccc19ff0b233c21760af49ed7
ring-ailing-ting <mimi121503@gmail.com>
2013-10-07 22:37:30 -0400

    updated javadocs, and added in numPlayers as parameter

--- MULE/src/GameConfig.java
+++ MULE/src/GameConfig.java
@@ -1,9 +1,24 @@
+/*
+* This is main configuration class for the M.U.L.E class. It holds various Game settings, including 
+* difficulty, setting the map size, and player number.
+* @author Wei Jiang
+* Version 1.0 10/7/2013
+* 
+*/
+
 public class GameConfig{
 	private int difficulty;
 	private int mapSize;
 	private int numPlayers;
+	
+	/*
+	* This is the constructor that instantiates the Game with a certain difficulty and sets a map.
+	* @difficulty - The difficulty of the game, ranging from 0, 1, 2. 
+	* @mapSize - The larger the mapSize value, the larger the map
+	* @numPlayers - The number of plays in the game 
+	*/
 
-	public GameConfig(int difficulty, int mapSize){//Difficulty: 0=beginner, 1=intermediate, 2=expert -- (premade)Map: 0=Small, 1=medium, 2=large
+	public GameConfig(int difficulty, int mapSize, int numPlayers){//Difficulty: 0=beginner, 1=intermediate, 2=expert -- (premade)Map: 0=Small, 1=medium, 2=large
 		//sets difficulty
 		if(difficulty>2 | difficulty<0){
 			this.difficulty=0;//if invalid, automatically sets difficulty to beginner
