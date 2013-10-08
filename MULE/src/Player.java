95c7d9e76a32602edf8c4e644bca66733310a6af
ring-ailing-ting <mimi121503@gmail.com>
2013-10-07 22:47:21 -0400

    Added javadoc, plus a money variable

--- MULE/src/Player.java
+++ MULE/src/Player.java
@@ -1,10 +1,25 @@
+/*
+* This is a player class that holds the player information, such as race, color, money amount, and name.
+* @author Wei Jiang
+* Version 1.0 10/7/2013
+*/
 public class Player{
 	private String name;
 	private int race;
 	private int color;
+	private int money;
+	
+	
+	/*
+	* This is the constructor that instantiates the Player
+	*@name - the name of the player
+	*@race - the race of the player
+	*@color - the color repesenting that player
+	*/
 
 	public Player(String name, int race, int color){
 		int i = 0;
+		int money=0;
 		this.name = name;
 
 		if(race<0 | race>4){//races' values fall between 0 and 4 inclusive. otherwise, defaults to race 0 (human?)
