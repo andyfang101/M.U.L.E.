cee8dbfb08ad34bf1c7625864a8a4d3009557935
cssdy <cassidybellmor@gmail.com>
2013-10-17 00:57:01 -0400

    Update Property.java

--- MULE/src/Property.java
+++ MULE/src/Property.java
@@ -9,7 +9,7 @@ public class Property extends JPanel{
 	private JComboBox purchaseProperty;
 	
 	private static final int WIDTH = 580;
-    private static final int HEIGHT = 600;
+    	private static final int HEIGHT = 600;
     
 	private boolean isPlayerTurn = true;
 	private boolean successful = false;
@@ -26,34 +26,34 @@ public class Property extends JPanel{
 	
 	public Property(JFrame frame) {
 		cont = false;
-        this.frame = frame;
-        setVisible(true);
-        setSize(WIDTH, HEIGHT);
-        
-        GroupLayout layout = new GroupLayout(this);
-        frame.setTitle("M.U.L.E. - Buy Property");
-        JPanel entire = new JPanel();
-        entire.setLayout(new GridLayout(3, 1));
-        add(entire);
-        
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2));
-        entire.add(panel);
-        
-        JLabel purchaseLabel = new JLabel("I would like to purchase : ");
+        	this.frame = frame;
+	        setVisible(true);
+	        setSize(WIDTH, HEIGHT);
+	        
+	        GroupLayout layout = new GroupLayout(this);
+	        frame.setTitle("M.U.L.E. - Buy Property");
+	        JPanel entire = new JPanel();
+	        entire.setLayout(new GridLayout(3, 1));
+	        add(entire);
+	        
+	        JPanel panel = new JPanel();
+	        panel.setLayout(new GridLayout(3, 2));
+	        entire.add(panel);
+	        
+	        JLabel purchaseLabel = new JLabel("I would like to purchase : ");
 
-        purchaseProperty = new JComboBox(properties);
-        purchaseProperty.addActionListener(new propListener());
-        
-        panel.add(purchaseLabel);
-        panel.add(purchaseProperty);
-        entire.add(panel);
-                
-        JButton cont = new JButton("Continue");
-        cont.addActionListener(new continueListener(frame));
-        entire.add(cont);
-        
-        frame.setContentPane(this);
+	        purchaseProperty = new JComboBox(properties);
+	        purchaseProperty.addActionListener(new propListener());
+	        
+	        panel.add(purchaseLabel);
+	        panel.add(purchaseProperty);
+	        entire.add(panel);
+	                
+	        JButton cont = new JButton("Continue");
+	        cont.addActionListener(new continueListener(frame));
+	        entire.add(cont);
+	        
+        	frame.setContentPane(this);
 	}	
 	
 	public void buyProperty(String property) {
@@ -107,19 +107,18 @@ public class Property extends JPanel{
         	this.frame = frame;
         }
         public void actionPerformed (ActionEvent event) {
-            cont = true;
+        	cont = true;
         }
     }
 	
 	private class propListener implements ActionListener {
-        public void actionPerformed (ActionEvent event) {
-            JComboBox p = (JComboBox)event.getSource();
-            String property = (String)p.getSelectedItem();
-            if (property.equals("Select property (temp)..."))
-            	System.out.println("Must select property.");
-            else {
-            	buyProperty(property);
-            }
-        }
+        	public void actionPerformed (ActionEvent event) {
+        		JComboBox p = (JComboBox)event.getSource();
+        		String property = (String)p.getSelectedItem();
+            		if (property.equals("Select property (temp)..."))
+            			System.out.println("Must select property.");
+            		else 
+            			buyProperty(property);
+        	}
 	}
 }
