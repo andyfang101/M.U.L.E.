f10fc2e018232254c8d774ad73d5e40157335c66
ring-ailing-ting <mimi121503@gmail.com>
2013-10-23 11:47:41 -0400

    javadoced

--- MULE/src/Plain.java
+++ MULE/src/Plain.java
@@ -1,10 +1,18 @@
 import java.awt.Point;
-
 import javax.swing.ImageIcon;
 
+/*
+* This class represents the Plain tile on the map
+* @author Eileen Wang
+* @Version 1.0, 10/20/13
+*/
 
 public class Plain extends Tile {
-
+	
+	/*
+	*The constructor for the plain Tile
+	*@Point p - the location of the tile on the map
+	*/
 	public Plain(Point location) {
 		super(location);
 		img = new ImageIcon("plain.png");
