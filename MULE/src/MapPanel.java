28b50f908d41aded26d5b1c272ee87b299183ea2
aardvarkone <andy.fang101@gmail.com>
2013-10-23 17:36:44 -0400

    Update MapPanel.java

--- MULE/src/MapPanel.java
+++ MULE/src/MapPanel.java
@@ -1,15 +1,23 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class MapPanel extends JPanel{
 	private Map map=null;
 	private GridLayout grid=null;
 	private boolean random; //implement random maps later
 	private JFrame frame;
-	
+	private JLabel curPlayerName;
+	private JButton done;
+	private static boolean isDone;
+	private int curTurn;
+	private Player curPlayer;
+
 	public MapPanel(boolean random, JFrame frame){
 		this.frame=frame;
 		this.random = random;
@@ -18,18 +26,52 @@ public class MapPanel extends JPanel{
 		grid = new GridLayout(Map.NUM_ROW, Map.NUM_COL);
 		setLayout(grid);
 		JButton[][] buttons = map.getMap();
-		
+
 		for(int r=0; r<buttons.length; r++){
-            if(buttons[r]!=null){
-           for(int c=0; c<buttons[r].length; c++){
-               if(buttons[r][c]!=null){
-            	   System.out.println(buttons[r][c].toString());
-                   add(buttons[r][c]);
-               }
-           }
-       }
-            
+			if(buttons[r]!=null){
+				for(int c=0; c<buttons[r].length; c++){
+					if(buttons[r][c]!=null){
+						System.out.println(buttons[r][c].toString());
+						add(buttons[r][c]);
+					}
+				}
+			}
+		}
+		add(curPlayerName);
+		done = new JButton("Done");
+		add(done);
+		done.addActionListener((ActionListener) new DListener());
+		curPlayerName.setText("Current Player: ");
+		curTurn = 0;
+		this.frame.setContentPane(this);
 	}
-	this.frame.setContentPane(this);
+	
+	public static void setIsDone(boolean isDonex){
+		isDone = isDonex;
+	}
+	
+	public boolean getIsDone(){
+		return isDone;
+	}
+	
+	public void setCurPlayer(Player p){
+		curPlayer = p;
+		curPlayerName.setText("Current Player: " + curPlayer.getName());
+	}
+	
+	public void nextTurn(){
+		curTurn++;
+	}
+	
+	public boolean gameOver(){
+		if(curTurn<12)
+			return false;
+		return true;
+	}
 }
+
+class DListener implements ActionListener{
+	public void actionPerformed(ActionEvent event) {
+		MapPanel.setIsDone(true);
+	}
 }
