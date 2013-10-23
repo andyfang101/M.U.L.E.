3ccc4f97ec25f1ab0db28f1093fa5008d356a855
ring-ailing-ting <mimi121503@gmail.com>
2013-10-23 11:48:11 -0400

    javadoced

--- MULE/src/River.java
+++ MULE/src/River.java
@@ -1,10 +1,18 @@
 import java.awt.Point;
 
 import javax.swing.ImageIcon;
-
+/*
+* This class represents the river tile on the map
+* @author Eileen Wang
+* @Version 1.0, 10/20/13
+*/
 
 public class River extends Tile {
-
+	
+	/*
+	*The constructor for the river Tile
+	*@Point p - the location of the tile on the map
+	*/
 	public River(Point location) {
 		super(location);
 		img = new ImageIcon("river.png");
