b0e1cf402f1ad1f93191e40fcbb5bdc818fc56c9
wjiang34 <wjiang34@gmail.com>
2013-11-05 21:32:29 -0800

    Update

--- MULE/src/SellPanel.java
+++ MULE/src/SellPanel.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,8 +10,21 @@ public class SellPanel extends Building{
 	private int FoodPrice,EnergyPrice,SmithorePrice,CrystitePrice;
 	private JLabel Sell,Food,Energy,Smithore,Crystite;
 	private JTextField foodNumber,energyNumber,smithoreNumber,crystiteNumber;
+	private JLabel pFood,pEnergy,pSmithore,pCrystite;
+	private JLabel pFoodN,pEnergyN,pSmithoreN,pCrystiteN;
+	private JPanel top,left,right,bottom,entire;
 	public SellPanel(GameMain frame, Player p, JPanel oldPanel) {
 		super(frame, p, oldPanel);
+		
+		top = new JPanel();
+		left = new JPanel();
+		right = new JPanel();
+		bottom = new JPanel();
+		entire = new JPanel();
+		
+		entire.setLayout(new BorderLayout());
+		left.setLayout(new GridLayout(0,2));
+		right.setLayout(new GridLayout(0,2));
 		
 		FoodPrice = 20;
 		EnergyPrice = 15;
@@ -23,25 +37,55 @@ public class SellPanel extends Building{
 		Smithore = new JLabel("Smithore:");
 		Crystite = new JLabel("Crystite:");
 		
+		
+		pFood = new JLabel("Your Food:");
+		pEnergy = new JLabel("Your Energy:");
+		pSmithore = new JLabel("Your Smithore:");
+		pCrystite = new JLabel("Your Crystite:");
+		
+		pFoodN = new JLabel(" " + p.getFood());
+		pEnergyN = new JLabel(" " + p.getEnergy());
+		pSmithoreN = new JLabel(" " + p.getSmithore());
+		pCrystiteN = new JLabel(" " + p.getCrystite());
+		
 		foodNumber = new JTextField("0");
 		energyNumber = new JTextField("0");
 		smithoreNumber = new JTextField("0");
 		crystiteNumber = new JTextField("0");
 		
 		JButton sell = new JButton("Sell");
-		JButton Back = new JButton("Back");
+		JButton back = new JButton("Back");
 		sell.addActionListener(new SellListener(p, frame, oldPanel));
-		add(Sell);
-		add(Food);
-		add(foodNumber);
-		add(Energy);
-		add(energyNumber);
-		add(Smithore);
-		add(smithoreNumber);
-		add(Crystite);
-		add(crystiteNumber);
-		add(sell);
+		back.addActionListener(new BackListener(p, frame, oldPanel));
+
+		top.add(Sell);
+		left.add(Food);
+		left.add(foodNumber);
+		left.add(Energy);
+		left.add(energyNumber);
+		left.add(Smithore);
+		left.add(smithoreNumber);
+		left.add(Crystite);
+		left.add(crystiteNumber);
 		
+		right.add(pFood);
+		right.add(pFoodN);
+		right.add(pEnergy);
+		right.add(pEnergyN);
+		right.add(pSmithore);
+		right.add(pSmithoreN);
+		right.add(pCrystite);
+		right.add(pCrystiteN);
+
+
+		bottom.add(sell);
+		bottom.add(back);
+
+		entire.add(top,BorderLayout.PAGE_START);
+		entire.add(left,BorderLayout.LINE_START);
+		entire.add(right,BorderLayout.LINE_END);
+		entire.add(bottom,BorderLayout.PAGE_END);
+		add(entire);
 	}
 	  private class SellListener implements ActionListener
 	    {
@@ -60,8 +104,37 @@ public class SellPanel extends Building{
 	    	   int so = Integer.parseInt(smithoreNumber.getText());
 	    	   int co = Integer.parseInt(crystiteNumber.getText());
 	    	   
-	    	   int amount = fd*FoodPrice + eg*EnergyPrice+ so*SmithorePrice+co*CrystitePrice;
-	    	   p.sell(fd, eg, so, co, amount);
+	    	   if (fd > p.getFood() || eg > p.getEnergy() || so > p.getSmithore() || co > p.getCrystite()){
+	    		   JOptionPane.showMessageDialog(null,
+						    "YOU DO NOT HAVE ENOUGH AMOUNT OF RESOURCES");
+	    	   }
+	    	   else{
+	    		   int amount = fd*FoodPrice + eg*EnergyPrice+ so*SmithorePrice+co*CrystitePrice;
+	    		   p.sell(fd, eg, so, co, amount);
+	    		   JOptionPane.showMessageDialog(null,
+						    "DONE!");
+	    	   }
+	    	   pFoodN.setText(" " + p.getFood());
+	    	   pEnergyN.setText(" " + p.getEnergy());
+	   		   pSmithoreN.setText(" " + p.getSmithore());
+	   		   pCrystiteN.setText(" " + p.getCrystite());
+
+	    	}
+	    }
+	  
+		private class BackListener implements ActionListener
+	    {
+	    	Player p;
+	    	GameMain frame;
+	    	JPanel oldpanel;
+			public BackListener(Player p, GameMain frame, JPanel oldpanel){
+	    		this.p=p;
+	    		this.frame=frame;
+	    		this.oldpanel=oldpanel;
+			}
+	       public void actionPerformed (ActionEvent event)
+	    	{
+	    	   frame.setContentPane(oldpanel);
 	    	}
 	    }
 }
