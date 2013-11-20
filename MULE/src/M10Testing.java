c5816b5a934da1102a1f53af6fbcb7f72ab93621
knigam <kushal.nigam1@gmail.com>
2013-11-20 02:49:19 -0500

    added test for genCharMap

--- MULE/src/M10Testing.java
+++ MULE/src/M10Testing.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -69,5 +73,70 @@ public class M10Testing {
 		Assert.assertFalse(p.buyProperty(t.getCost(), t));
 	}
 	
-
+	
+	//Tests for genCharMap in Map class
+	/**
+	 * creates a new, map with default layout and checks validity
+	 * @author kushal
+	 */
+	@Test
+	public void genDefaultCharMap(){
+		char[][] mpRep={{'g','g','g','m','m','p','r','p','g'},{'g','g','g','g','m','m','m','m','g'},{'g','g','p','p','t','r','r','r','g'},{'g','g','g','g','g','p','p','g','g'},{'r','r','r','r','p','p','g','g','g'}};
+		Map map = new Map(false, frame);
+		char[][] mapRep = map.getMapRep();
+		boolean hasTown = false, hasOneTown = false, mapsMatch = false;
+		if(mapRep.length == mpRep.length && mapRep[0].length == mpRep[0].length){
+			mapsMatch = true;
+			for(int i = 0; i < mapRep.length; i++){
+				for(int j = 0; j < mapRep[i].length; j++){
+					if(mapRep[i][j] != mpRep[i][j])
+						mapsMatch = false;
+					if(mapRep[i][j] == 't' && hasTown == false){
+						hasOneTown = true;
+						hasTown = true;
+					}
+					else if(mapRep[i][j] == 't' && hasTown == true)
+						hasOneTown = false;
+					
+				}
+			}
+		}
+		Assert.assertTrue(hasOneTown && mapsMatch);
+	}
+	
+	/**
+	 * creates a new, map with random layout and checks validity
+	 * @author kushal
+	 */
+	@Test
+	public void genRandomCharMap(){
+		
+		ArrayList<Character> mapRepTypes = new ArrayList<Character>();
+		mapRepTypes.add('m');
+		mapRepTypes.add('g');
+		mapRepTypes.add('t');
+		mapRepTypes.add('r');
+		mapRepTypes.add('p');
+			
+		Map map = new Map(true, frame);
+		char[][] mapRep = map.getMapRep();
+		boolean hasTown = false, hasOneTown = false, mapsMatch = false;
+		if(mapRep.length == map.NUM_ROW && mapRep[0].length == Map.NUM_COL){
+			mapsMatch = true;
+			for(int i = 0; i < mapRep.length; i++){
+				for(int j = 0; j < mapRep[i].length; j++){
+					if(!mapRepTypes.contains(mapRep[i][j]))
+						mapsMatch = false;
+					if(mapRep[i][j] == 't' && hasTown == false){
+						hasOneTown = true;
+						hasTown = true;
+					}
+					else if(mapRep[i][j] == 't' && hasTown == true)
+						hasOneTown = false;
+					
+				}
+			}
+		}
+		Assert.assertTrue(hasOneTown && mapsMatch);
+	}
 }
